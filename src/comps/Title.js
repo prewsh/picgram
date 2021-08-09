13b622476387271eeb93ca9608dea351d8548ca8
prewsh <pngwube@gmail.com>
2021-08-09 08:04:19 +0100

    Edited the title again

--- src/comps/Title.js
+++ src/comps/Title.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Title = () => {
   return (
     <div className="title">
-      <h1>PicGram</h1>
+      <h1>Album</h1>
       <h2>Upload and View your Pictures</h2>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
     </div>
